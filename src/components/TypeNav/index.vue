31f5d7139aa51825b7d65b218ffa771c958f1518
sh <aotushi@hotmail>
2021-01-29 13:19:50 +0800

    03-1

--- src/components/TypeNav/index.vue
+++ src/components/TypeNav/index.vue
@@ -2,7 +2,51 @@
     <!-- 商品分类导航 -->
     <div class="type-nav">
         <div class="container">
-            <h2 class="all">全部商品分类</h2>
+            <!-- 新建一个div 包含h2和sort -->
+            <div @mouseleave="currentIndex=-1"> 
+                <h2 class="all">全部商品分类</h2>
+                <div class="sort">
+                <div class="all-sort-list2">
+                    <div
+                        class="item"
+                        :class="{ item_on: currentIndex === index }"
+                        v-for="(c1, index) in categoryList"
+                        :key="c1.categoryId"
+                        @mouseenter="moveInItem(index)"
+                    >
+                        <h3>
+                            <!-- <a href="">图书、音像、数字商品</a> -->
+                            <a href="">{{ c1.categoryName }}</a>
+                        </h3>
+                        <div class="item-list clearfix">
+                            <div class="subitem">
+                                <dl
+                                    class="fore"
+                                    v-for="(c2, index) in c1.categoryChild"
+                                    :key="c2.categoryId"
+                                >
+                                    <dt>
+                                        <!-- <a href="">电子书</a> -->
+                                        <a href="">{{ c2.categoryName }}</a>
+                                    </dt>
+                                    <dd>
+                                        <em
+                                            v-for="(c3,
+                                            index) in c2.categoryChild"
+                                            :key="c3.categoryId"
+                                        >
+                                            <!-- <a href="">婚恋/两性</a> -->
+                                            <a href="">{{ c3.categoryName }}</a>
+                                        </em>
+                                    </dd>
+                                </dl>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            
             <nav class="nav">
                 <a href="###">服装城</a>
                 <a href="###">美妆馆</a>
@@ -13,184 +57,180 @@
                 <a href="###">有趣</a>
                 <a href="###">秒杀</a>
             </nav>
-            <div class="sort">
-                <div class="all-sort-list2">
-                    <div class="item" v-for="(c1, index) in categoryList" :key='c1.categoryId'>
-                        <h3>
-                            <!-- <a href="">图书、音像、数字商品</a> -->
-                            <a href="">{{c1.categoryName}}</a>
-                        </h3>
-                        <div class="item-list clearfix">
-                            <div class="subitem">
-                                <dl class="fore" v-for="(c2, index) in c1.categoryChild" :key="c2.categoryId">
-                                    <dt>
-                                        <!-- <a href="">电子书</a> -->
-                                        <a href="">{{c2.categoryName}}</a>
-                                    </dt>
-                                    <dd>
-                                        <em v-for="(c3, index) in c2.categoryChild" :key="c3.categoryId">
-                                            <!-- <a href="">婚恋/两性</a> -->
-                                            <a href="">{{c3.categoryName}}</a>
-                                        </em>
-                                    </dd>
-                                </dl>
-                            </div>
-                        </div>
-                    </div>
-                   
-                </div>
-            </div>
+
         </div>
     </div>
 </template>
 
 <script>
-import {mapState} from 'vuex';
+import { mapState } from "vuex";
+// import _ from 'lodash'; //这是引入会把整个lodash全部引入,打包后体积比较大
+import throttle from 'lodash/throttle';
 export default {
-    name:"TypeNav",
-    mounted(){
-         
-        console.log(this)
-        this.$store.dispatch('getCategoryList');
-        
+    name: "TypeNav",
+    data() {
+        return {
+            currentIndex: -1,
+        };
     },
-//从vuex中把数据拿到vue组件中. 
-//以后只要从vuex中拿数据(state, getters),都在computed中拿
-//以后只要从vuex中拿数据(actions, mutations),都在methods中拿
+    methods:{
+        // _.throttle(renewToken, 300000, { 'trailing': false });
 
+        //没有节流的时候
+        // moveInItem(index){
+        //     this.currentIndex = index
+        // }
 
-    computed:{
+        //节流 传递的函数不能使用箭头函数,其内部的this不是组件实例对象
+        // 节流问题: 1.鼠标在20毫秒内移走,最后一次的调用才出来: 使用第三个参数trailing  . 2.时间太长出现卡顿:计算机运行是微秒,20毫秒相对微秒已经很长
+        moveInItem: throttle(
+            function(index){
+                this.currentIndex=index;
+                console.log(index)
+            },
+            20,
+            { 'trailing': false }
+        )
+    },
+    mounted() {
+        console.log(this);
+        this.$store.dispatch("getCategoryList");
+    },
+    //从vuex中把数据拿到vue组件中.
+    //以后只要从vuex中拿数据(state, getters),都在computed中拿
+    //以后只要从vuex中拿数据(actions, mutations),都在methods中拿
+
+    computed: {
         //两种形式;数组写法的条件
         ...mapState({
-            categoryList:state=>state.home.categoryList
-        })
-    }
-
+            categoryList: (state) => state.home.categoryList,
+        }),
+    },
 };
 </script>
 
 <style lang="less">
- .type-nav {
-        border-bottom: 2px solid #e1251b;
+.type-nav {
+    border-bottom: 2px solid #e1251b;
 
-        .container {
-            width: 1200px;
-            margin: 0 auto;
-            display: flex;
-            position: relative;
+    .container {
+        width: 1200px;
+        margin: 0 auto;
+        display: flex;
+        position: relative;
 
-            .all {
-                width: 210px;
+        .all {
+            width: 210px;
+            height: 45px;
+            background-color: #e1251b;
+            line-height: 45px;
+            text-align: center;
+            color: #fff;
+            font-size: 14px;
+            font-weight: bold;
+        }
+
+        .nav {
+            a {
                 height: 45px;
-                background-color: #e1251b;
+                margin: 0 22px;
                 line-height: 45px;
-                text-align: center;
-                color: #fff;
-                font-size: 14px;
-                font-weight: bold;
+                font-size: 16px;
+                color: #333;
             }
+        }
 
-            .nav {
-                a {
-                    height: 45px;
-                    margin: 0 22px;
-                    line-height: 45px;
-                    font-size: 16px;
-                    color: #333;
-                }
-            }
+        .sort {
+            position: absolute;
+            left: 0;
+            top: 45px;
+            width: 210px;
+            height: 461px;
+            position: absolute;
+            background: #fafafa;
+            z-index: 999;
 
-            .sort {
-                position: absolute;
-                left: 0;
-                top: 45px;
-                width: 210px;
-                height: 461px;
-                position: absolute;
-                background: #fafafa;
-                z-index: 999;
+            .all-sort-list2 {
+                .item {
+                    h3 {
+                        line-height: 30px;
+                        font-size: 14px;
+                        font-weight: 400;
+                        overflow: hidden;
+                        padding: 0 20px;
+                        margin: 0;
 
-                .all-sort-list2 {
-                    .item {
-                        h3 {
-                            line-height: 30px;
-                            font-size: 14px;
-                            font-weight: 400;
-                            overflow: hidden;
-                            padding: 0 20px;
-                            margin: 0;
-
-                            a {
-                                color: #333;
-                            }
+                        a {
+                            color: #333;
                         }
+                    }
 
-                        .item-list {
-                            display: none;
-                            position: absolute;
-                            width: 734px;
-                            min-height: 460px;
-                            background: #f7f7f7;
-                            left: 210px;
-                            border: 1px solid #ddd;
-                            top: 0;
-                            z-index: 9999 !important;
+                    .item-list {
+                        display: none;
+                        position: absolute;
+                        width: 734px;
+                        min-height: 460px;
+                        background: #f7f7f7;
+                        left: 210px;
+                        border: 1px solid #ddd;
+                        top: 0;
+                        z-index: 9999 !important;
 
-                            .subitem {
-                                float: left;
-                                width: 650px;
-                                padding: 0 4px 0 8px;
+                        .subitem {
+                            float: left;
+                            width: 650px;
+                            padding: 0 4px 0 8px;
 
-                                dl {
-                                    border-top: 1px solid #eee;
-                                    padding: 6px 0;
+                            dl {
+                                border-top: 1px solid #eee;
+                                padding: 6px 0;
+                                overflow: hidden;
+                                zoom: 1;
+
+                                &.fore {
+                                    border-top: 0;
+                                }
+
+                                dt {
+                                    float: left;
+                                    width: 54px;
+                                    line-height: 22px;
+                                    text-align: right;
+                                    padding: 3px 6px 0 0;
+                                    font-weight: 700;
+                                }
+
+                                dd {
+                                    float: left;
+                                    width: 415px;
+                                    padding: 3px 0 0;
                                     overflow: hidden;
-                                    zoom: 1;
 
-                                    &.fore {
-                                        border-top: 0;
-                                    }
-
-                                    dt {
+                                    em {
                                         float: left;
-                                        width: 54px;
-                                        line-height: 22px;
-                                        text-align: right;
-                                        padding: 3px 6px 0 0;
-                                        font-weight: 700;
-                                    }
-
-                                    dd {
-                                        float: left;
-                                        width: 415px;
-                                        padding: 3px 0 0;
-                                        overflow: hidden;
-
-                                        em {
-                                            float: left;
-                                            height: 17px;
-                                            line-height: 17px;
-                                            padding: 0 8px;
-                                            margin-top: 5px;
-                                            border-left: 1px solid #ccc;
-                                            &:hover{
-                                                background:#ddd;
-                                            }
+                                        height: 17px;
+                                        line-height: 17px;
+                                        padding: 0 8px;
+                                        margin-top: 5px;
+                                        border-left: 1px solid #ccc;
+                                        &:hover {
+                                            background: #ddd;
                                         }
                                     }
                                 }
                             }
                         }
+                    }
 
-                        &:hover {
-                            background:#ddd;
-                            .item-list {
-                                display: block;
-                            }
+                    &.item_on {
+                        background: #ddd;
+                        .item-list {
+                            display: block;
                         }
                     }
                 }
             }
         }
     }
+}
 </style>
